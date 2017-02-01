bf0007005b3f526bdfe58896f5a68f5eb9a5ded2
negebauer <negebauer@uc.cl>
2017-02-01 16:34:43 -0300

    added deploy beta as default deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,17 +2,18 @@ echo 'Checking git status...'
 # if [ ! -z  $(git status --porcelain) ]; then echo 'Git repository dirty. Clean it'; exit; fi
 echo 'Git clean!'
 
-echo 'Bump major, minor, patch or none\n$ \c'
+echo 'Bump major, minor, patch or none (default)\n$ \c'
 read bump
-if [ ! "$bump" == 'none' ] && [ ! "$bump" == "" ]; then version=$(npm version $bump); echo "Version bumped to $version"; else echo 'No bump'; fi
+if [ ! "$bump" == 'none' ] && [ ! "$bump" == '' ]; then version=$(npm version $bump); echo "Version bumped to $version"; else echo 'No bump'; fi
 
-echo 'Deploy alpha beta store\n$ \c'
+echo 'Deploy alpha beta (default) store\n$ \c'
 read deploy
+if [ "$deploy" == '' ]; then deploy="beta"; fi
 
-echo 'iOS deploy'
+echo "iOS deploy: Running fastlane $deploy"
 cd ios
 fastlane $deploy
 
-# echo 'Android deploy'
+# echo "Android deploy: Running fastlane $deploy"
 # cd ../android
 # fastlane $deploy
