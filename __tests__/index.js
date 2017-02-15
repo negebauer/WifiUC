61fc3e4ad1d02542c7a34cdac7a5b5103ba89c0d
negebauer <negebauer@uc.cl>
2017-02-15 10:48:35 -0300

    working on tests

---
+++ __tests__/index.js
@@ -0,0 +1,16 @@
+import 'react-native'
+import React from 'react'
+import Home from '../src/components/Home'
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer'
+
+it('works?', () => {
+  return new Promise((res, rej) => {
+    res()
+  })
+})
+
+it('renders correctly', () => {
+  const tree = renderer.create(<Home/>);
+})
