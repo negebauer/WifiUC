54855f6be610f344d9e05a8e84ff1b2065e9d37b
negebauer <negebauer@uc.cl>
2017-02-15 19:24:29 -0300

    test android with key

--- scripts/ci_dependencies.sh
+++ scripts/ci_dependencies.sh
@@ -8,6 +8,7 @@ npm run generate_env $WIFIUC_USER $WIFIUC_PASS
 # Android
 brew install android-sdk
 $ANDROID_HOME/tools/android update sdk --no-ui
+security add-generic-password -a android_keystore -l android_keystore -s android_keystore -w $WIFIUC_ANDROID_KEYSTORE
 
 # Deploy
 sudo gem update fastlane
