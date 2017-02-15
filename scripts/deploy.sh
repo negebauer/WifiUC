b817d5f1a0f60b5d5b9f6b406a0016d150e31b70
negebauer <negebauer@uc.cl>
2017-02-15 11:52:33 -0300

    build on travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ echo 'Deploy alpha beta (default) store\n$ \c'
 read deploy
 if [ "$deploy" == '' ]; then deploy="beta"; fi
 
-if [ ! "$bump" == 'none' ] && [ ! "$bump" == '' ]; then version=$(npm version $bump); echo "Version bumped to $version"; else npm run build; echo 'Bump build only'; fi
+if [ ! "$bump" == 'none' ] && [ ! "$bump" == '' ]; then version=$(npm version $bump); echo "Version bumped to $version"; else npm run bump_build; echo 'Bump build only'; fi
 
 echo "iOS deploy: Running fastlane $deploy"
 cd ios
