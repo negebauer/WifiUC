61bfb964c4ec7bc06a54498e2ca62fc27b873b33
negebauer <negebauer@uc.cl>
2017-02-15 19:17:07 -0300

    test circle deploy

--- scripts/ci_dependencies.sh
+++ scripts/ci_dependencies.sh
@@ -1,6 +1,13 @@
+# React native stuff
 brew reinstall node
 npm install
 npm install -g react-native-cli
 npm run postinstall
 npm run generate_env $WIFIUC_USER $WIFIUC_PASS
+
+# Android
+brew install android-sdk
+$ANDROID_HOME/tools/android update sdk --no-ui
+
+# Deploy
 sudo gem update fastlane
