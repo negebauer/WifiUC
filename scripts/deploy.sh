5ae524c67dd9d681efb043a86240206f481b0e6b
negebauer <negebauer@uc.cl>
2017-02-15 13:55:24 -0300

    allow bump and deploy in script. try deploy with travis!

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,20 @@ echo 'Checking git status...'
 if [ ! -z  $(git status --porcelain) ]; then echo 'Git repository dirty. Clean it'; exit; fi
 echo 'Git clean!'
 
-echo 'Bump major, minor, patch or none (default)\n$ \c'
-read bump
+if [ ! -z $1 ]; then
+  bump=$1
+else
+  echo 'Bump major, minor, patch or none (default)\n$ \c'
+  read bump
+fi
 
-echo 'Deploy alpha beta (default) store\n$ \c'
-read deploy
+if [ ! -z $2 ]; then
+  deploy=$2
+else
+  echo 'Deploy alpha beta (default) store\n$ \c'
+  read deploy
+fi
+
 if [ "$deploy" == '' ]; then deploy="beta"; fi
 
 if [ ! "$bump" == 'none' ] && [ ! "$bump" == '' ]; then version=$(npm version $bump); echo "Version bumped to $version"; else npm run bump_build; echo 'Bump build only'; fi
