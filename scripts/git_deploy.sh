01b54651d848d95f49c5b4ae23f7a5ee819ec5ef
negebauer <negebauer@uc.cl>
2017-02-19 00:40:03 -0300

    Fix git deploy script

--- scripts/git_deploy.sh
+++ scripts/git_deploy.sh
@@ -1,7 +1,7 @@
 git_commit=$(git log -1 --pretty=%B)
 if [[ $git_commit == '[deploy] alpha' ]]; then
   echo 'alpha'
-elif [[ $git_commit == '[deploy] alpha' ]]; then
+elif [[ $git_commit == '[deploy] beta' ]]; then
   echo 'beta'
 else
   echo 'none'
