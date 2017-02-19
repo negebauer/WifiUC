f2d1fb2d56d0985ead93a7b3ebf7d035d09e50b1
negebauer <negebauer@uc.cl>
2017-02-19 01:14:00 -0300

    Get google credentials from certificates repo

--- scripts/android_get_keystore.sh
+++ scripts/android_get_keystore.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/negebauer/certificates.git temp_certificates
 mv -f temp_certificates/android/wifiuc/release.keystore android/app
+mv -f temp_certificates/android/wifiuc/google_credentials.json android/app/fastlane
+
 rm -rf temp_certificates
