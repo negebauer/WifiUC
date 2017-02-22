d8d677d8d69e8243964d78dd8a928d0dbd01957a
negebauer <negebauer@uc.cl>
2017-02-22 18:46:41 -0300

    User action creator test

--- src/redux/user/index.spec.js
+++ src/redux/user/index.spec.js
@@ -1,5 +1,20 @@
 import * as user from './'
 
-console.log(user)
+const userData = {
+  username: 'test',
+  password: 'pass'
+}
 
-it('passes', () => expect(1).toBe(1))
+describe('user actions', () => {
+  it('login should create LOGIN action', () => {
+    expect(user.login(userData)).toEqual({type: user.LOGIN, user: userData})
+  })
+
+  it('logout should create LOGOUT action', () => {
+    expect(user.logout()).toEqual({type: user.LOGOUT})
+  })
+
+  it('userUpdate should create USER_UPDATE action', () => {
+    expect(user.userUpdate(userData)).toEqual({type: user.USER_UPDATE, user: userData})
+  })
+})
