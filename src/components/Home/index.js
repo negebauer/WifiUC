26b2420ee6599f9ea994e57b253fb04128291e7b
negebauer <negebauer@uc.cl>
2017-02-23 14:02:44 -0300

    Home component handles login

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,33 @@
 import React, {Component} from 'react'
-import {View, Text, Button} from 'react-native'
+import {View, Text, Button, ActivityIndicator, StyleSheet} from 'react-native'
 import Config from 'react-native-config'
 
-const Home = ({devices, user, userUpdate, login, logout}) => (
-  <View>
-    {Object.keys(user).map(key => <Text key={key}>{`${key}: ${user[key]}`}</Text>)}
-    <Button title='Login bad' onPress={() => login({username: 'nope', password: 'wrong'})}/>
-    <Button title='Login good' onPress={() => login({username: Config.WIFIUC_USER, password: Config.WIFIUC_PASS})}/>
-    <Button title='Logout' onPress={() => logout()}/>
-  </View>
-)
+class Home extends Component {
+  componentDidMount() {
+    this.props.login(this.props.user)
+  }
+
+  render() {
+    const {devices, user, userUpdate, login, logout} = this.props
+    if (user.loading) {
+      return <View style={styles.containerCentered}><ActivityIndicator/></View>
+    }
+    if (user.error) {
+      return <View style={styles.container}>
+        <Text>YOU SHOULD LOGIN</Text>
+        <Button title='Login good' onPress={() => login({username: Config.WIFIUC_USER, password: Config.WIFIUC_PASS})}/>
+      </View>
+    }
+    return (
+      <View>
+        {Object.keys(user).map(key => <Text key={key}>{`${key}: ${user[key]}`}</Text>)}
+        <Button title='Login bad' onPress={() => login({username: 'nope', password: 'wrong'})}/>
+        <Button title='Login good' onPress={() => login({username: Config.WIFIUC_USER, password: Config.WIFIUC_PASS})}/>
+        <Button title='Logout' onPress={() => logout()}/>
+      </View>
+    )
+  }
+}
 
 Home.propTypes = {
   devices: React.PropTypes.object.isRequired,
@@ -18,5 +36,17 @@ Home.propTypes = {
   login: React.PropTypes.func.isRequired,
   logout: React.PropTypes.func.isRequired
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16
+  },
+  containerCentered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignSelf: 'center'
+  }
+})
 
 export default Home
