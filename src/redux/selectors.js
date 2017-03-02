dcd9cac92c4a4d234df0fd0e6e9a4cfb69fef6bf
negebauer <negebauer@uc.cl>
2017-03-02 14:26:11 -0300

    Fix network activity selector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,19 +2,36 @@ import {createSelector} from 'reselect'
 
 const getDevices = state => state.devices
 const getDevicesIgnoreKeys = state => ['loading', 'error', 'editing', 'adding']
-const getDevicesOnly = createSelector(getDevices, getDevicesIgnoreKeys, (devices, ignore) => Object.keys(devices).reduce((obj, key) => {
-  if (ignore.indexOf(key) === -1) {
-    return {
-      ...obj,
-      [key]: devices[key]
+const getDevicesOnly = createSelector(getDevices, getDevicesIgnoreKeys, (
+  devices,
+  ignore
+) =>
+  Object.keys(devices).reduce((obj, key) => {
+    if (ignore.indexOf(key) === -1) {
+      return {
+        ...obj,
+        [key]: devices[key],
+      }
     }
-  }
-  return obj
-}, {}))
-export const devicesList = createSelector(getDevicesOnly, devices => Object.values(devices))
-export const devicesActivity = createSelector(devicesList, devices => devices.map(device => device.loading).length > 0)
+    return obj
+  }, {}))
+export const devicesList = createSelector(
+  getDevicesOnly,
+  devices => Object.values(devices)
+)
+export const devicesActivity = createSelector(
+  devicesList,
+  devices => devices.map(device => device.loading).filter(Boolean).length > 0
+)
 
 const getUser = state => state.user
-export const userActivity = createSelector(getUser, user => user.loading || !user.rehydrated)
+export const userActivity = createSelector(
+  getUser,
+  user => user.loading || !user.rehydrated
+)
 
-export const networkActivity = createSelector(userActivity, devicesActivity, (userA, devicesA) => userA || devicesA)
+export const networkActivity = createSelector(
+  userActivity,
+  devicesActivity,
+  (userA, devicesA) => userA || devicesA
+)
